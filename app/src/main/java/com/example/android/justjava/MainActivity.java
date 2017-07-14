df4ecb35732ce6675828db639c91883802477f8d
syafiudinn <moh.syafiudin@outlook.com>
2017-07-14 21:50:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.android.justjava;
+
+import android.annotation.TargetApi;
+import java.text.NumberFormat;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import static android.R.id.message;
+
+public class MainActivity extends AppCompatActivity {
+
+    int quantity = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view) {
+        int harga = quantity *2000;
+        String message = "tOTAL :Rp"+harga;
+        displayMessage(message);
+    }
+    private void display(int number){
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText(""+number);
+    }
+
+    private void displayprice(int number){
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+
+    }
+    private void displayMessage(String message){
+        TextView priceTextView = (TextView)findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+
+    public void increment(View view) {
+        quantity = quantity + 1;
+        display(quantity);
+    }
+
+    public void decrement(View view) {
+        quantity = quantity -1;
+        display(quantity);
+    }
+}
